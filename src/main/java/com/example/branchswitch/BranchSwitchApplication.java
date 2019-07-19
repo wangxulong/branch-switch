11eb23b2a4f32821cb855a70ea66c8d2a3ed5c07
wangxulong <duanxian0402@126.com>
2019-07-19 17:41:16 +0800

    init commit

---
+++ src/main/java/com/example/branchswitch/BranchSwitchApplication.java
@@ -0,0 +1,13 @@
+package com.example.branchswitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchSwitchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BranchSwitchApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/branchswitch/IndexController.java
@@ -0,0 +1,11 @@
+package com.example.branchswitch;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    public String index(){
+        return "master";
+    }
+}
