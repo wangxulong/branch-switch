39ce2367c7fc461dbd01f918d72150003c18dae4
wangxulong <duanxian0402@126.com>
2019-07-26 13:01:18 +0800

    master commit

--- src/main/java/com/example/branchswitch/IndexController.java
+++ src/main/java/com/example/branchswitch/IndexController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     public String index(){
-        return "master";
+        return "master pre";
     }
 }
